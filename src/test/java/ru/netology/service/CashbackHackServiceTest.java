699d82dad53fec8288dc625f00a541a0543d67af
Slava netologia <s-29@mail.ru>
2024-08-10 12:11:56 +0300

    Задача 1 Часть 2 - добавлен тест

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+import junit.framework.TestCase;
+public class CashbackHackServiceTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testRemain() {
+    }
+}
